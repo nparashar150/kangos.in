8c145e653fda9af559cdc02d82c44e8ce3f8c871
nparashar150 <nparashar150@gmail.com>
2021-02-28 16:10:41 +0530

    Format Index.html

--- Code/Js.js
+++ Code/Js.js
@@ -1,8 +1,8 @@
 function navit() {
-    var x = document.getElementById("navbarid");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
+  var x = document.getElementById("navbarid");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
 }
